86255d7c2d84645352097e6d49928e0e4554ae98
Apisart Chanpuang <c.apisart@gmail.com>
2021-07-04 14:26:11 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    count += 2
 })
+let count = 0
+basic.showIcon(IconNames.Heart)
+music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+basic.pause(1000)
+music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Forever)
+for (let index = 0; index < 1; index++) {
+    basic.showString("Hello!")
+}
+while (true) {
+    basic.showNumber(count)
+    if (count == 10) {
+        break;
+    }
+}
+basic.showIcon(IconNames.Happy)
+music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
